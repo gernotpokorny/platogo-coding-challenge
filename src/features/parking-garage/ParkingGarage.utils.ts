6057af4ad2a03f08d21a4a1e2856372f526dab24
Gernot Pokorny <gernot.pokorny@etron.at>
2023-02-28 02:07:51 +0100

    Small change within calculateTicketState()

--- src/features/parking-garage/ParkingGarage.utils.ts
+++ src/features/parking-garage/ParkingGarage.utils.ts
@@ -35,7 +35,7 @@ export const getFormattedPaymentDate = (paymentDate: Date) => {
 };
 
 export const calculateTicketState = (ticket: Ticket, currentDate: Date) => {
-	if (ticket.payments) {
+	if (ticket.payments && ticket.payments.length > 0) {
 		const paymentDate = new Date(ticket.payments[ticket.payments.length - 1].paymentDate);
 		const minutes = Math.abs(currentDate.getTime() - paymentDate.getTime()) / 60000 // Dividing by 60000 converts the milliseconds difference into minutes.
 		return minutes > 15 ? TicketState.UNPAID : TicketState.PAID;
