cdc482079f9c767f35a3beb843fc6a36c86a0a1b
Gernot Pokorny <gernot.pokorny@etron.at>
2023-02-28 01:44:14 +0100

    Added documentation to calculateTicketPrice()
    Also changed a parameter name.

--- src/features/parking-garage/ParkingGarage.utils.ts
+++ src/features/parking-garage/ParkingGarage.utils.ts
@@ -11,8 +11,12 @@ export const generateBarCode = () => {
 	return generateRandomNumber(16);
 };
 
-export const calculateTicketPrice = (issueDate: Date, paymentDate: Date) => {
-	const hours = Math.abs(paymentDate.getTime() - issueDate.getTime()) / 36e5; // 36e5 is the scientific notation for 60*60*1000, dividing by which converts the milliseconds difference into hours.
+/**
+ * `date` can be for example the date of the issuance of the ticket or the date of the last payment which happened before the current payment.
+ * `paymentDate` is the current payment.
+ */
+export const calculateTicketPrice = (date: Date, paymentDate: Date) => {
+	const hours = Math.abs(paymentDate.getTime() - date.getTime()) / 36e5; // 36e5 is the scientific notation for 60*60*1000, dividing by which converts the milliseconds difference into hours.
 	const billedHours = Math.ceil(hours);
 	const HOURLY_RATE = 2;
 	return billedHours * HOURLY_RATE;
